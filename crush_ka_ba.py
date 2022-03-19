788f46f6cf855f3269218ecb1c1cb5aae03de53b
Abbey <oohisabel23@gmail.com>
2022-03-19 21:35:48 +0800

    Create crush_ka_ba.py

---
+++ crush_ka_ba.py
@@ -0,0 +1,8 @@
+print("Crush ka ba ng crush mo?")
+while True:
+    answer = input("Answer: ")
+    if answer == "hindi":
+        print("I THINK TAMA")
+        break
+    else:
+        print("AMFEE")
